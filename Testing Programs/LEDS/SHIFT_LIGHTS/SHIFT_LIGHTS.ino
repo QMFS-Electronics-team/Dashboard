880f134cd5fb4a0c8eb3895d806f76b11ccd3d8d
Bhargav Maniar <bhargav22478@gmail.com>
2024-07-25 00:54:24 +0100

    Add shift lights example code

--- Testing Programs/LEDS/SHIFT_LIGHTS/SHIFT_LIGHTS.ino
+++ Testing Programs/LEDS/SHIFT_LIGHTS/SHIFT_LIGHTS.ino
@@ -1,58 +1,58 @@
 #include <FastLED.h>
 
-#define LED_PIN     4
-#define NUM_LEDS    30
-#define BRIGHTNESS  64
-#define SHIFT_DELAY 40
-#define COLOR_ORDER GRB
-#define LED_TYPE    WS2811
+#define LED_PIN       4
+#define NUM_LEDS      30
+#define BRIGHTNESS    64
+#define SHIFT_DELAY   1
+#define MAX_SHIFT_RPM 3000
+#define COLOR_ORDER   GRB
+#define LED_TYPE      WS2811
+
+int rpmLightInterval = MAX_SHIFT_RPM / NUM_LEDS;
 
 CRGB leds[NUM_LEDS];
 
-void turnOffLEDs() {
-  for(int i = 0; i <= NUM_LEDS; i++) {
-    leds[i].setRGB(0,0,0); 
+void setRPMLights(int rpmValue) {
+  for (int i = 0; i <= NUM_LEDS; i++) {
+    if (rpmValue >= (i + 1)*rpmLightInterval) {
+      if (i < 10) {               // LEDs should be Green
+        leds[i].setRGB(0, BRIGHTNESS, 0);
+        delay(SHIFT_DELAY);
+      } else if (i < 20) {        // LEDs should be Red
+        leds[i].setRGB(BRIGHTNESS, 0, 0);
+        delay(SHIFT_DELAY);
+      } else if (i < 30) {        // LEDs should be Blue
+        leds[i].setRGB(0, 0, BRIGHTNESS);
+        delay(SHIFT_DELAY);
+      }
+      FastLED.show();
+    } else {
+      leds[i].setRGB(0, 0, 0);
+      FastLED.show();
+    }
   }
-  delay(10); 
-  FastLED.show();
 }
 
-void simulateRPMLights(){
- for(int i = 0; i < NUM_LEDS; i++) {
-
-  // LEDs should be Green
-  if(i < 10) {
-     leds[i].setRGB(0,BRIGHTNESS,0);
-     delay(SHIFT_DELAY);
-     FastLED.show();
+void simulateRPMIncrease() {
+  for (int rpm = 0; rpm < 3500; rpm += 100) {
+    setRPMLights(rpm);
   }
+}
 
-  // LEDs should be Red
-  else if(i < 20) {
-    leds[i].setRGB(BRIGHTNESS,0,0);
-    delay(SHIFT_DELAY);
-    FastLED.show();
+void simulateRPMDecrease() {
+  for (int rpm = 3500; rpm > 0; rpm -= 100) {
+    setRPMLights(rpm);
   }
-
-  // LEDs should be Blue
-  else if(i < 30) {
-    leds[i].setRGB(0,0,BRIGHTNESS);
-    delay(SHIFT_DELAY);
-    FastLED.show();
-  }
-  
- }
- 
 }
 
 void setup() {
-  delay(3000); 
+  delay(1000);
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 }
 
 void loop() {
-  simulateRPMLights();
-  delay(1000); 
-  turnOffLEDs();
-  delay(4000);
+  simulateRPMIncrease();
+  delay(2000);
+  simulateRPMDecrease();
+  delay(2000);
 }
