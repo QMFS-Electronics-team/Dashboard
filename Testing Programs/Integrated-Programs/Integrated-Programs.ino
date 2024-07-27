366e95014de8a7e0218fd08d28ac6e4bbf8aa5cb
Bhargav Maniar <bhargav22478@gmail.com>
2024-07-27 16:57:23 +0100

    Update LED code to use the RPM Lights code

--- Testing Programs/Integrated-Programs/Integrated-Programs.ino
+++ Testing Programs/Integrated-Programs/Integrated-Programs.ino
@@ -45,11 +45,15 @@
 #define HORIZONTAL 3
 
 // LEDs
-#define NUM_LEDS    30
-#define BRIGHTNESS  64
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
-#define UPDATES_PER_SECOND 100
+#define LED_PIN       4
+#define NUM_LEDS      30
+#define BRIGHTNESS    64
+#define SHIFT_DELAY   1
+#define MAX_SHIFT_RPM 3000
+#define COLOR_ORDER   GRB
+#define LED_TYPE      WS2811
+
+
 
 // A sample NMEA stream.
 const char *gpsStream =
@@ -117,12 +121,11 @@ const int RXPin = 34;
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
 // LEDs
+int rpmLightInterval = MAX_SHIFT_RPM / NUM_LEDS;
 CRGB leds[NUM_LEDS];
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
 
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+// RPM Lights simulation
+boolean rpmState = true;
 
 // SD Card (Write)
 String outputString;
@@ -130,7 +133,6 @@ String outputString;
 // CAN BUS
 struct can_frame canMsg;
 MCP2515 mcp2515(2);
-
 
 
 
@@ -238,12 +240,8 @@ void setup_compass(void) {
 }
 
 void setup_leds() {
-  delay(3000); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
+  delay(1000); // Delay powerup
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 }
 
 void set_display_data() {
@@ -479,137 +477,38 @@ void get_can_bus_data() {
 // LED Related
 //----------------
 
-void set_leds()
-{
-  ChangePalettePeriodically();
-
-  static uint8_t startIndex = 0;
-  startIndex = startIndex + 1; /* motion speed */
-
-  FillLEDsFromPaletteColors( startIndex);
-
-  FastLED.show();
-  FastLED.delay(1000 / UPDATES_PER_SECOND);
-}
-
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
-{
-  uint8_t brightness = 255;
-
-  for ( int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-    colorIndex += 3;
-  }
-}
-
-void ChangePalettePeriodically()
-{
-  uint8_t secondHand = (millis() / 1000) % 60;
-  static uint8_t lastSecond = 99;
-
-  if ( lastSecond != secondHand) {
-    lastSecond = secondHand;
-    if ( secondHand ==  0)  {
-      currentPalette = RainbowColors_p;
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 10)  {
-      currentPalette = RainbowStripeColors_p;
-      currentBlending = NOBLEND;
-    }
-    if ( secondHand == 15)  {
-      currentPalette = RainbowStripeColors_p;
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 20)  {
-      SetupPurpleAndGreenPalette();
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 25)  {
-      SetupTotallyRandomPalette();
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 30)  {
-      SetupBlackAndWhiteStripedPalette();
-      currentBlending = NOBLEND;
-    }
-    if ( secondHand == 35)  {
-      SetupBlackAndWhiteStripedPalette();
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 40)  {
-      currentPalette = CloudColors_p;
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 45)  {
-      currentPalette = PartyColors_p;
-      currentBlending = LINEARBLEND;
-    }
-    if ( secondHand == 50)  {
-      currentPalette = myRedWhiteBluePalette_p;
-      currentBlending = NOBLEND;
-    }
-    if ( secondHand == 55)  {
-      currentPalette = myRedWhiteBluePalette_p;
-      currentBlending = LINEARBLEND;
+void setRPMLights(int rpmValue) {
+  for (int i = 0; i <= NUM_LEDS; i++) {
+    if (rpmValue >= (i + 1)*rpmLightInterval) {
+      if (i < 10) {               // LEDs should be Green
+        leds[i].setRGB(0, BRIGHTNESS, 0);
+        delay(SHIFT_DELAY);
+      } else if (i < 20) {        // LEDs should be Red
+        leds[i].setRGB(BRIGHTNESS, 0, 0);
+        delay(SHIFT_DELAY);
+      } else if (i < 30) {        // LEDs should be Blue
+        leds[i].setRGB(0, 0, BRIGHTNESS);
+        delay(SHIFT_DELAY);
+      }
+      FastLED.show();
+    } else {
+      leds[i].setRGB(0, 0, 0);
+      FastLED.show();
     }
   }
 }
 
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-  for ( int i = 0; i < 16; i++) {
-    currentPalette[i] = CHSV( random8(), 255, random8());
+void simulateRPMIncrease() {
+  for (int rpm = 0; rpm < 3500; rpm += 100) {
+    setRPMLights(rpm);
   }
 }
 
-void SetupBlackAndWhiteStripedPalette()
-{
-  // 'black out' all 16 palette entries...
-  fill_solid( currentPalette, 16, CRGB::Black);
-  // and set every fourth one to white.
-  currentPalette[0] = CRGB::White;
-  currentPalette[4] = CRGB::White;
-  currentPalette[8] = CRGB::White;
-  currentPalette[12] = CRGB::White;
-
+void simulateRPMDecrease() {
+  for (int rpm = 3500; rpm > 0; rpm -= 100) {
+    setRPMLights(rpm);
+  }
 }
-
-void SetupPurpleAndGreenPalette()
-{
-  CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-  CRGB green  = CHSV( HUE_GREEN, 255, 255);
-  CRGB black  = CRGB::Black;
-
-  currentPalette = CRGBPalette16(
-                     green,  green,  black,  black,
-                     purple, purple, black,  black,
-                     green,  green,  black,  black,
-                     purple, purple, black,  black );
-}
-
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-  CRGB::Red,
-  CRGB::Gray, // 'white' is too bright compared to red and blue
-  CRGB::Blue,
-  CRGB::Black,
-
-  CRGB::Red,
-  CRGB::Gray,
-  CRGB::Blue,
-  CRGB::Black,
-
-  CRGB::Red,
-  CRGB::Red,
-  CRGB::Gray,
-  CRGB::Gray,
-  CRGB::Blue,
-  CRGB::Blue,
-  CRGB::Black,
-  CRGB::Black
-};
 
 
 
@@ -755,7 +654,7 @@ void setup() {
   setup_compass();
   setup_gps();
   setup_leds();
-  set_leds();
+  setRPMLights(0);
   set_display_data();
   Serial.println("Dashboard Setup Complete");
 }
@@ -775,7 +674,18 @@ void loop() {
 
   get_compass_data();
   get_three_axis_gyro_data();
-  set_leds();
+
+
+  if(rpmState) {
+    simulateRPMIncrease();
+    delay(200);
+    rpmState = !rpmState;
+  } else {
+    simulateRPMDecrease();
+    delay(200);
+    rpmState = !rpmState;
+  }
+  
   get_can_bus_data();
   delay(500);
 }
