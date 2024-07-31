cfb18f64cefe3ddf631b01c55538e39cd57503a1
Bhargav Maniar <bhargav22478@gmail.com>
2024-07-31 14:04:01 +0100

    Remove un-used code

--- Testing Programs/Integrated-Programs/Integrated-Programs.ino
+++ Testing Programs/Integrated-Programs/Integrated-Programs.ino
@@ -46,15 +46,6 @@
 #define COLOR_ORDER   GRB
 #define LED_TYPE      WS2811
 
-// A sample NMEA stream.
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
-
 //----------------
 // Libraries
 //----------------
@@ -479,11 +470,10 @@ void simulateRPMDecrease() {
 void simulateRPMLights() {
     if(rpmState) {
     simulateRPMIncrease();
-    delay(200);
   } else {
     simulateRPMDecrease();
-    delay(200);
   }
+  delay(200);
   rpmState = !rpmState;
 }
 
