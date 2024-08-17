0c22b9da4b19a92ffcca9105dad56c02397d5ca6
Bhargav Maniar <bhargav22478@gmail.com>
2024-08-17 15:00:06 +0100

    Move references from .ino file to .md file

--- Testing Programs/Integrated-Programs/Integrated-Programs.ino
+++ Testing Programs/Integrated-Programs/Integrated-Programs.ino
@@ -1,18 +1,4 @@
 //----------------
-// References
-//----------------
-// Display
-// https://techtutorialsx.com/2021/01/31/esp32-ili9341-display-hello-world/#Testing_the_code
-// https://arduino.stackexchange.com/questions/50576/elegant-solution-for-refreshing-tft-display-content
-// GPS
-// https://arduinogetstarted.com/tutorials/arduino-gps
-// LEDs
-// https://raw.githubusercontent.com/RuiSantosdotme/Random-Nerd-Tutorials/master/Projects/Arduino_WS2812B_Color_Palette.ino
-// CAN BUS
-// https://copperhilltech.com/blog/esp32-triple-can-bus-application-through-adding-two-mcp2515-ports/
-
-
-//----------------
 // Libraries
 //----------------
 
