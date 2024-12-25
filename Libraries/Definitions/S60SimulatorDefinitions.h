7837244428759d6f05c84da7b45711a9994649f4
Bhargav Maniar <bhargav22478@gmail.com>
2024-12-25 00:28:15 +0000

    Add header file for S60 Simulator and CAN testing code

---
+++ Libraries/Definitions/S60SimulatorDefinitions.h
@@ -0,0 +1,14 @@
+
+// Potentiometers
+#define KNOB_ONE    A0
+#define KNOB_TWO    A1
+#define KNOB_THREE  A2
+#define KNOB_FOUR   A3
+#define KNOB_FIVE   A4
+#define KNOB_SIX    A5
+#define KNOB_SEVEN  A6
+
+// PINs
+#define CANBUS_CS 10
+
+#define BAUDRATE 115200
