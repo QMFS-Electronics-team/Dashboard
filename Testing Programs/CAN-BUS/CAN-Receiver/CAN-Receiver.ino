5fb6283b7181ce17a63f368bfa054fa3716c696b
Bhargav Maniar <bhargav22478@gmail.com>
2024-12-26 00:00:07 +0000

    Update CAN BUS testing code

--- Testing Programs/CAN-BUS/CAN-Receiver/CAN-Receiver.ino
+++ Testing Programs/CAN-BUS/CAN-Receiver/CAN-Receiver.ino
@@ -1,38 +1,47 @@
 // Reference: https://how2electronics.com/interfacing-mcp2515-can-bus-module-with-arduino/
-#include <SPI.h>              // Library for using SPI Communication 
-#include <mcp2515.h>          // Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
- 
+#include <SPI.h>                      // Library for using SPI Communication 
+#include <mcp2515.h>                  // Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
+#include <S60SimulatorDefinitions.h>  // Preprocessor for this code
+
+
 struct can_frame canMsg;
+MCP2515 mcp2515(CANBUS_CS);
  
-MCP2515 mcp2515(2);                 // SPI CS Pin 2
- 
- 
-void setup()
-{
-  Serial.begin(9600);                // Begins Serial Communication at 9600 baudrate
-  SPI.begin();                       // Begins SPI communication
-   
+void setup() {
+  while(!Serial);
+  Serial.begin(BAUDRATE);
+  Serial.println(F("CAN BUS Receiver"));
+  
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); // Set CAN at speed 500KBPS and Clock 8MHz
-  mcp2515.setNormalMode();                   // Set CAN at normal mode
+  int can_setup = mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+  
+  if(can_setup == MCP2515::ERROR_OK) {
+    Serial.println("CAN BUS Initialised");
+  } else {
+    Serial.println("CAN BUS Not Initialised. Error.");
+  }
 }
  
  
 void loop() {
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+  if (mcp2515.readMessage(&canMsg) != MCP2515::ERROR_NOMSG) {
 
     Serial.print("CAN Message ID: ");
-    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(canMsg.can_id); // print ID
     Serial.print(" ");
     Serial.print("Message Length: ");
-    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(canMsg.can_dlc); // print DLC
     Serial.print(" ");
     Serial.print("Data: ");
     for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
-      Serial.print(canMsg.data[i],HEX);
+      Serial.print(canMsg.data[i]);
       Serial.print(" ");
     }
-
     Serial.println();      
+  } else {
+    Serial.println("No Data Recieved");
+//    delay(1000);
   }
+//  delay(100);
 }
--- Testing Programs/CAN-BUS/CAN-Simulator-S60/CAN-Simulator-S60.ino
+++ Testing Programs/CAN-BUS/CAN-Simulator-S60/CAN-Simulator-S60.ino
@@ -1,6 +1,7 @@
 // Reference: https://how2electronics.com/interfacing-mcp2515-can-bus-module-with-arduino/
-#include <SPI.h>          // Library for using SPI Communication 
-#include <mcp2515.h>      // Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
+#include <SPI.h>                      // Library for using SPI Communication 
+#include <mcp2515.h>                  // Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
+#include <S60SimulatorDefinitions.h>  // Preprocessor for this code
 
 struct can_frame canMsg2000;
 struct can_frame canMsg2001;
@@ -9,35 +10,32 @@ struct can_frame canMsg2003;
 struct can_frame canMsg2004;
 struct can_frame canMsg2005;
 
-#define KNOB_ONE    A0
-#define KNOB_TWO    A1
-#define KNOB_THREE  A2
-#define KNOB_FOUR   A3
-#define KNOB_FIVE   A4
-#define KNOB_SIX    A5
-#define KNOB_SEVEN  A6
-
-MCP2515 mcp2515(10);
+MCP2515 mcp2515(CANBUS_CS);
 
 void setup() {
-  while (!Serial);
-  Serial.begin(115200);
-  SPI.begin();
-
+  while(!Serial);
+  Serial.begin(BAUDRATE);
+  Serial.println(F("CAN BUS S60 Simulator"));
+  
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); // Sets CAN at speed 500KBPS and Clock 8MHz
+  int can_setup = mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
-
-  Serial.println(F("CAN-Simulator-S60"));
+  
+  if(can_setup == MCP2515::ERROR_OK) {
+    Serial.println("CAN BUS Initialised");
+  } else {
+    Serial.println("CAN BUS Not Initialised. Error.");
+  }
 }
 
-void sendPacket2000(int water_temp, int tps, int rpm) {
+
+void sendPacket2000() {
   canMsg2000.can_id  = 0x2000;
   canMsg2000.can_dlc = 4;
-  canMsg2000.data[0] = rpm;         // RPM
-  canMsg2000.data[1] = tps;         // TPS %
-  canMsg2000.data[2] = water_temp;  // Water Temp C
-  canMsg2000.data[3] = 0x20;        // Air Temp C
+  canMsg2000.data[0] = get_rpm();         // RPM
+  canMsg2000.data[1] = get_tps();         // TPS %
+  canMsg2000.data[2] = get_water_temp();  // Water Temp C
+  canMsg2000.data[3] = 0x20;              // Air Temp C
 
   Serial.println(F("Sending Packet 2000"));
   mcp2515.sendMessage(&canMsg2000);
@@ -55,37 +53,37 @@ void sendPacket2001() {
   mcp2515.sendMessage(&canMsg2001);
 }
 
-void sendPacket2002(int oil_temp, int battery_voltage) {
+void sendPacket2002() {
   canMsg2002.can_id  = 0x2002;
   canMsg2002.can_dlc = 4;
-  canMsg2002.data[0] = 0x03;            // Fuel P Kpa
-  canMsg2002.data[1] = oil_temp;        // Oil Temp C
-  canMsg2002.data[2] = battery_voltage; // Volts x 10
-  canMsg2002.data[3] = 0x20;            // Fuel Con. L/100Km x 10
+  canMsg2002.data[0] = 0x03;                  // Fuel P Kpa
+  canMsg2002.data[1] = get_oil_temp();        // Oil Temp C
+  canMsg2002.data[2] = get_battery_voltage(); // Volts x 10
+  canMsg2002.data[3] = 0x20;                  // Fuel Con. L/100Km x 10
 
   Serial.println(F("Sending Packet 2002"));
   mcp2515.sendMessage(&canMsg2002);
 }
 
-void sendPacket2003(int gear) {
+void sendPacket2003() {
   canMsg2003.can_id  = 0x2003;
   canMsg2003.can_dlc = 4;
-  canMsg2003.data[0] = gear; // Gear
-  canMsg2003.data[1] = 0x02; // Advance Degx10
-  canMsg2003.data[2] = 0x03; // Injection ms x 100
-  canMsg2003.data[3] = 0x02; // Fuel Con. L/Hr x 10
+  canMsg2003.data[0] = get_gear(); // Gear
+  canMsg2003.data[1] = 0x02;       // Advance Degx10
+  canMsg2003.data[2] = 0x03;       // Injection ms x 100
+  canMsg2003.data[3] = 0x02;       // Fuel Con. L/Hr x 10
 
   Serial.println(F("Sending Packet 2003"));
   mcp2515.sendMessage(&canMsg2003);
 }
 
-void sendPacket2004(int bps) {
+void sendPacket2004() {
   canMsg2004.can_id  = 0x2004;
   canMsg2004.can_dlc = 4;
-  canMsg2004.data[0] = bps;  // Ana1 mV - BPS
-  canMsg2004.data[1] = 0x02; // Ana2 mV
-  canMsg2004.data[2] = 0x03; // Ana3 mV
-  canMsg2004.data[3] = 0x05; // Cam Advance x 10
+  canMsg2004.data[0] = get_bps();  // Ana1 mV - BPS
+  canMsg2004.data[1] = 0x02;       // Ana2 mV
+  canMsg2004.data[2] = 0x03;       // Ana3 mV
+  canMsg2004.data[3] = 0x05;       // Cam Advance x 10
 
   Serial.println(F("Sending Packet 2004"));
   mcp2515.sendMessage(&canMsg2004);
@@ -136,6 +134,7 @@ int get_bps() {
   return int(map(analogRead(KNOB_THREE), 0, 1023, 100, 0));
 }
 
+
 void logOutput() {
   Serial.println(F(""));
   Serial.print(F("Water Temp - "));
@@ -155,12 +154,13 @@ void logOutput() {
   Serial.println(F(""));
 }
 
+
 void loop() {
-  sendPacket2000(get_water_temp(), get_tps(), get_rpm());
+  sendPacket2000();
   sendPacket2001();
-  sendPacket2002(get_oil_temp(), get_battery_voltage());
-  sendPacket2003(get_gear());
-  sendPacket2004(get_bps());
+  sendPacket2002();
+  sendPacket2003();
+  sendPacket2004();
   sendPacket2005();
   
   logOutput();
