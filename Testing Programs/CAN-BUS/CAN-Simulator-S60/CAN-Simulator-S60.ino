c4497c1a8e31193ebd56e36e28691ab8e8bd1734
Bhargav Maniar <bhargav22478@gmail.com>
2024-12-29 02:13:33 +0000

    Add support for 8 bytes per packet

--- Testing Programs/CAN-BUS/CAN-Simulator-S60/CAN-Simulator-S60.ino
+++ Testing Programs/CAN-BUS/CAN-Simulator-S60/CAN-Simulator-S60.ino
@@ -10,9 +10,18 @@ struct can_frame canMsg2003;
 struct can_frame canMsg2004;
 struct can_frame canMsg2005;
 
+byte higher_byte; 
+byte lower_byte;
+
 MCP2515 mcp2515(CANBUS_CS);
  
 int send_delay = 1/SEND_FREQUENCY;
+
+int rpm, tps, water_temp = 0;       // Packet 2000 - [0] RPM, [1] Throttle Position Sensor, [2] Water Temperature
+int kph = 0;                        // Packet 2001 - [2] Speed reported by ECU
+int oil_temp, battery_voltage = 0;  // Packet 2002 - [1] Oil Temperature, [2] Battery Voltage
+int gear = 0;                       // Packet 2003 - [0] Gear
+int bps = 0;  
 
 void setup() {
   while(!Serial);
@@ -31,11 +40,32 @@ void setup() {
 
 void sendPacket2000() {
   canMsg2000.can_id  = 0x2000| CAN_EFF_FLAG;
-  canMsg2000.can_dlc = 4;
-  canMsg2000.data[0] = get_rpm();         // RPM
-  canMsg2000.data[1] = get_tps();         // TPS %
-  canMsg2000.data[2] = get_water_temp();  // Water Temp C
-  canMsg2000.data[3] = 0x20;              // Air Temp C
+  canMsg2000.can_dlc = 8;
+ 
+  // RPM
+  rpm = get_rpm();
+  higher_byte = (rpm >> 8) & 0xFF;
+  lower_byte = rpm & 0xFF;
+  canMsg2000.data[0] = higher_byte;
+  canMsg2000.data[1] = lower_byte;
+
+  // TPS %
+  tps = get_tps();
+  higher_byte = (tps >> 8) & 0xFF;
+  lower_byte = tps & 0xFF;
+  canMsg2000.data[2] = higher_byte;
+  canMsg2000.data[3] = lower_byte;
+
+  // Water Temp C
+  water_temp = get_rpm();
+  higher_byte = (water_temp >> 8) & 0xFF;
+  lower_byte = water_temp & 0xFF;
+  canMsg2000.data[4] = higher_byte;
+  canMsg2000.data[5] = lower_byte;
+
+  // Air Temp C
+  canMsg2000.data[6] = 0x00;
+  canMsg2000.data[7] = 0x00;
 
   Serial.println(F("Sending Packet 2000"));
   mcp2515.sendMessage(&canMsg2000);
@@ -43,11 +73,23 @@ void sendPacket2000() {
 
 void sendPacket2001() {
   canMsg2001.can_id  = 0x2001 | CAN_EFF_FLAG;
-  canMsg2001.can_dlc = 4;
-  canMsg2001.data[0] = 0x01; // MAP Kpa
-  canMsg2001.data[1] = 0x01; // Lambda x 1000
-  canMsg2001.data[2] = 0x06; // KPH x 10
-  canMsg2001.data[3] = 0x02; // Oil P Kpa
+  canMsg2001.can_dlc = 8;
+  
+  // MAP Kpa
+  canMsg2001.data[0] = 0x00;
+  canMsg2001.data[1] = 0x00;
+
+  // Lambda x 1000
+  canMsg2001.data[2] = 0x00;
+  canMsg2001.data[3] = 0x00;
+  
+  // KPH x 10
+  canMsg2001.data[4] = 0x00;
+  canMsg2001.data[5] = 0x00;
+
+  // Oil P Kpa
+  canMsg2001.data[6] = 0x00;
+  canMsg2001.data[7] = 0x00;
 
   Serial.println(F("Sending Packet 2001"));
   mcp2515.sendMessage(&canMsg2001);
@@ -55,11 +97,29 @@ void sendPacket2001() {
 
 void sendPacket2002() {
   canMsg2002.can_id  = 0x2002 | CAN_EFF_FLAG;
-  canMsg2002.can_dlc = 4;
-  canMsg2002.data[0] = 0x03;                  // Fuel P Kpa
-  canMsg2002.data[1] = get_oil_temp();        // Oil Temp C
-  canMsg2002.data[2] = get_battery_voltage(); // Volts x 10
-  canMsg2002.data[3] = 0x20;                  // Fuel Con. L/100Km x 10
+  canMsg2002.can_dlc = 8;
+  
+  // Fuel P Kpa
+  canMsg2002.data[0] = 0x00;
+  canMsg2002.data[1] = 0x00;                  
+  
+  // Oil Temp C
+  oil_temp = get_oil_temp();
+  higher_byte = (oil_temp >> 8) & 0xFF;
+  lower_byte = oil_temp & 0xFF;        
+  canMsg2002.data[2] = higher_byte;
+  canMsg2002.data[3] = lower_byte;
+  
+  // Volts x 10
+  battery_voltage = get_battery_voltage();
+  higher_byte = (battery_voltage >> 8) & 0xFF;
+  lower_byte = battery_voltage & 0xFF;        
+  canMsg2002.data[4] = higher_byte;
+  canMsg2002.data[5] = lower_byte;
+  
+  // Fuel Con. L/100Km x 10
+  canMsg2002.data[6] = 0x00;
+  canMsg2002.data[7] = 0x00;                  
 
   Serial.println(F("Sending Packet 2002"));
   mcp2515.sendMessage(&canMsg2002);
@@ -67,11 +127,26 @@ void sendPacket2002() {
 
 void sendPacket2003() {
   canMsg2003.can_id  = 0x2003 | CAN_EFF_FLAG;
-  canMsg2003.can_dlc = 4;
-  canMsg2003.data[0] = get_gear(); // Gear
-  canMsg2003.data[1] = 0x02;       // Advance Degx10
-  canMsg2003.data[2] = 0x03;       // Injection ms x 100
-  canMsg2003.data[3] = 0x02;       // Fuel Con. L/Hr x 10
+  canMsg2003.can_dlc = 8;
+
+  // Gear
+  gear = get_gear();
+  higher_byte = (gear >> 8) & 0xFF;
+  lower_byte = gear & 0xFF;
+  canMsg2003.data[0] = higher_byte;
+  canMsg2003.data[1] = lower_byte;
+
+  // Advance Degx10
+  canMsg2003.data[2] = 0x00;
+  canMsg2003.data[3] = 0x00;
+
+  // Injection ms x 100
+  canMsg2003.data[4] = 0x00;
+  canMsg2003.data[5] = 0x00;       
+  
+  // Fuel Con. L/Hr x 10
+  canMsg2003.data[6] = 0x00;
+  canMsg2003.data[7] = 0x00;       
 
   Serial.println(F("Sending Packet 2003"));
   mcp2515.sendMessage(&canMsg2003);
@@ -79,11 +154,23 @@ void sendPacket2003() {
 
 void sendPacket2004() {
   canMsg2004.can_id  = 0x2004 | CAN_EFF_FLAG;
-  canMsg2004.can_dlc = 4;
-  canMsg2004.data[0] = get_bps();  // Ana1 mV - BPS
-  canMsg2004.data[1] = 0x02;       // Ana2 mV
-  canMsg2004.data[2] = 0x03;       // Ana3 mV
-  canMsg2004.data[3] = 0x05;       // Cam Advance x 10
+  canMsg2004.can_dlc = 8;
+
+  // Ana1 mV - BPS
+  bps = get_bps();
+  higher_byte = (bps >> 8) & 0xFF;
+  lower_byte = bps & 0xFF;
+  canMsg2004.data[0] = higher_byte;
+  canMsg2004.data[1] = lower_byte;
+  
+  // Ana2 mV
+  canMsg2004.data[1] = 0x00;
+  
+  // Ana3 mV
+  canMsg2004.data[2] = 0x00;
+
+  // Cam Advance x 10
+  canMsg2004.data[3] = 0x00;
 
   Serial.println(F("Sending Packet 2004"));
   mcp2515.sendMessage(&canMsg2004);
@@ -91,11 +178,23 @@ void sendPacket2004() {
 
 void sendPacket2005() {
   canMsg2005.can_id  = 0x2005 | CAN_EFF_FLAG;
-  canMsg2005.can_dlc = 4;
-  canMsg2005.data[0] = 0x10; // Cam Targ x 10
-  canMsg2005.data[1] = 0x10; // CAM PWM x 10
-  canMsg2005.data[2] = 0x00; // Crank Errors
-  canMsg2005.data[3] = 0x00; // CAM Errors
+  canMsg2005.can_dlc = 8;
+  
+  // Cam Targ x 10
+  canMsg2005.data[0] = 0x00; 
+  canMsg2005.data[1] = 0x00; 
+  
+  // CAM PWM x 10
+  canMsg2005.data[2] = 0x00;
+  canMsg2005.data[3] = 0x00; 
+  
+  // Crank Errors
+  canMsg2005.data[4] = 0x00;
+  canMsg2005.data[5] = 0x00; 
+
+  // CAM Errors
+  canMsg2005.data[6] = 0x00;
+  canMsg2005.data[7] = 0x00; 
 
   Serial.println(F("Sending Packet 2005"));
   mcp2515.sendMessage(&canMsg2005);
