9d379f4cef810545aa702f236862ade64b59c89a
Bhargav Maniar <bhargav22478@gmail.com>
2025-01-09 20:34:50 +0000

    Disable serial for CAN BUS and GPS. Disable updates to Info screen

--- Dashboard_Programs/Dashboard_Program/Dashboard_Program.ino
+++ Dashboard_Programs/Dashboard_Program/Dashboard_Program.ino
@@ -293,12 +293,12 @@ void display_update_task(void *pvParameters) {
       lv_label_set_text(ui_MainScreen_Label_LabelTime, timeOutput.c_str());
 
       // Info Screen
-      displaySpeedAccuracy = "Speed Accuracy: " + String(speedAccuracy/1000.0) + "mph";
-      displayGPSAccuracy = "GPS Accuracy: " + String(sqrt((verticalAccuracy*verticalAccuracy)/1000000.0 + (horizontalAccuracy*horizontalAccuracy)/1000000.0)) + "m";
-      lv_label_set_text(ui_InfoScreen_Label_LabelBluetoothRSSI,  displayBLERSSI.c_str());
-      lv_label_set_text(ui_InfoScreen_Label_LabelSpeedAcc, displaySpeedAccuracy.c_str());
-      lv_label_set_text(ui_InfoScreen_Label_LabelGPSAcc, displayGPSAccuracy.c_str());
-      lv_label_set_text(ui_InfoScreen_Label_LabelDataRate, "GPS Data Rate: 25Hz"); // Keep This Default 25Hz
+      // displaySpeedAccuracy = "Speed Accuracy: " + String(speedAccuracy/1000.0) + "mph";
+      // displayGPSAccuracy = "GPS Accuracy: " + String(sqrt((verticalAccuracy*verticalAccuracy)/1000000.0 + (horizontalAccuracy*horizontalAccuracy)/1000000.0)) + "m";
+      // lv_label_set_text(ui_InfoScreen_Label_LabelBluetoothRSSI,  displayBLERSSI.c_str());
+      // lv_label_set_text(ui_InfoScreen_Label_LabelSpeedAcc, displaySpeedAccuracy.c_str());
+      // lv_label_set_text(ui_InfoScreen_Label_LabelGPSAcc, displayGPSAccuracy.c_str());
+      // lv_label_set_text(ui_InfoScreen_Label_LabelDataRate, "GPS Data Rate: 25Hz"); // Keep This Default 25Hz
 
     } else {
 
@@ -716,10 +716,10 @@ void ui_reset() {
   lv_bar_set_value(ui_MainScreen_Bar_BarTPS, 0, LV_ANIM_OFF);
   lv_bar_set_value(ui_MainScreen_Bar_BarBPS, 0, LV_ANIM_OFF);
   lv_bar_set_value(ui_MainScreen_Bar_BarRPM, 0, LV_ANIM_OFF);
-  lv_label_set_text(ui_InfoScreen_Label_LabelBluetoothRSSI, "Bluetooth RSSI:     ");
-  lv_label_set_text(ui_InfoScreen_Label_LabelDataRate, "GPS Data Rate:     ");
-  lv_label_set_text(ui_InfoScreen_Label_LabelSpeedAcc, "GPS Accuracy:     ");
-  lv_label_set_text(ui_InfoScreen_Label_LabelGPSAcc, "GPS Accuracy:     ");
+  // lv_label_set_text(ui_InfoScreen_Label_LabelBluetoothRSSI, "Bluetooth RSSI:     ");
+  // lv_label_set_text(ui_InfoScreen_Label_LabelDataRate, "GPS Data Rate:     ");
+  // lv_label_set_text(ui_InfoScreen_Label_LabelSpeedAcc, "GPS Accuracy:     ");
+  // lv_label_set_text(ui_InfoScreen_Label_LabelGPSAcc, "GPS Accuracy:     ");
   return;
 }
 
--- Libraries/DashboardProjectHeaderFiles/Dashboard.h
+++ Libraries/DashboardProjectHeaderFiles/Dashboard.h
@@ -37,8 +37,8 @@
 #define BPS_PCB_EN                   0 // When 1, the BPS is read from the PCB Pin and not the CAN BUS
 #define NETURAL_DETECT_PCB_EN        0 // When 1, the Netural is determined by the PCB Pin and not the CAN BUS
 #define ECU_TYPE 					 1 // 1 For S60 ECU, 0 For Standard ECU
-#define CAN_BUS_SERIAL_OUTPUT_EN     1
-#define BLE_GPS_SERIAL_OUTPUT_EN     1
+#define CAN_BUS_SERIAL_OUTPUT_EN     0
+#define BLE_GPS_SERIAL_OUTPUT_EN     0
 #define CAN_BUS_SD_CARD_LOGGING_EN 	 1
 #define GPS_SD_CARD_LOGGING_EN       1
 
